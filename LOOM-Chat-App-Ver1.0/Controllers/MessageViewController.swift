12c5d085a4409c65693422fa7398769c2946dca5
Miki Nomura <miki@fyi.tokyo>
2018-04-08 10:59:38 +0900

    Added UITabViews

---
+++ LOOM-Chat-App-Ver1.0/Controllers/MessageViewController.swift
@@ -0,0 +1,14 @@
+//
+//  MessageViewController.swift
+//  LOOM-Chat-App-Ver1.0
+//
+//  Created by Miki Nomura on 4/8/18.
+//  Copyright © 2018 Miki Nomura. All rights reserved.
+//
+
+import Foundation
+import UIKit
+
+class MessageViewController: UIViewController {
+    
+}
