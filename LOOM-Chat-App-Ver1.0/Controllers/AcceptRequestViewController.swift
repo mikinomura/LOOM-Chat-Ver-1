dd3b49edc551cd4f0c0881fb03152da1c084295e
Miki Nomura <miki@fyi.tokyo>
2018-04-15 15:09:45 +0900

    Added a room name

--- LOOM-Chat-App-Ver1.0/Controllers/AcceptRequestViewController.swift
+++ LOOM-Chat-App-Ver1.0/Controllers/AcceptRequestViewController.swift
@@ -25,12 +25,45 @@ class AcceptRequestViewController: UIViewController {
         // Change the firebase status
         ChangeFollowingStatus()
         
+        // Create a conversation room name
+        CreateRoomName()
+        
         // Go to the main screen
+        let initialViewController = UIStoryboard.initialViewController(for: .main)
+        self.view.window?.rootViewController = initialViewController
+        self.view.window?.makeKeyAndVisible()
     }
     
     private func ChangeFollowingStatus(){
         let ref = Database.database().reference().child("usersInfo").child(User.current.username).child("followingPartner")
         let childUpdates = ["status": true]
         ref.updateChildValues(childUpdates)
+    }
+    
+    private func CreateRoomName() {
+        let username = User.current.username
+        let ref = Database.database().reference().child("usersInfo").child(username)
+        let followingRef = ref.child("followingPartner")
+        let roomRef = ref.child(byAppendingPath: "room")
+        
+        var roomName = ""
+        followingRef.observeSingleEvent(of: .value, with: { (snapshot) in
+            // Get user value
+            let value = snapshot.value as? NSDictionary
+            let partnerUsername = value?["username"] as? String ?? ""
+            
+            let partnerRef = Database.database().reference().child("usersInfo").child(partnerUsername)
+            let partnerRoomRef = partnerRef.child(byAppendingPath: "room")
+            
+            roomName = username + partnerUsername
+            let roomNameAttr = ["roomName": roomName]
+            
+            // Set values to the user and the partner's room
+            roomRef.setValue(roomNameAttr)
+            partnerRoomRef.setValue(roomNameAttr)
+            
+        }) { (error) in
+            print(error.localizedDescription)
+        }
     }
 }
